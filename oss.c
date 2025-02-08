59537a426e6632b1fc6c06acac83e09a74087539
Nicholas <nickhieb77@gmail.com>
2025-02-08 12:27:02 -0600

    command line parsing and running children

--- oss.c
+++ oss.c
@@ -7,28 +7,64 @@
 
 int main(int argc, char *argv[])
 {
-    pid_t childPid = fork();
-    if (argc == 1 || (argc == 2 && strcmp(argv[1], "-h") == 0))
+    int proc;
+    int simul;
+    int completedChilds = 0;
+    int runningChilds = 0;
+    int status;
+    char *iter = NULL;
+    
+    // Help Argument and how to use.
+    if (argc < 3 || (argc == 2 && strcmp(argv[1], "-h") == 0))
     {
         printf("Usage: %s [-h] [-n <number>] [-s <number>] [-t <number>]", argv[0]);
-        printf("Option: -h\tPrints this help message.\n");
+        printf("Option: -h  \tPrints this help message.\n");
         return 0;
     }
 
-    if (childPid == 0)
+    // Command line arguments and parsing them for input.
+    for (int i = 1; i < argc; i++)
     {
-        char *args[] = {"./user", argv[1], NULL};
-        execvp(args[0], args);
-    }
-    else
-    {
-        wait(NULL);
-        printf("Parent Process. My PID is: %d My Child PID is: %d\n", getpid(), childPid);
-        return EXIT_SUCCESS;
+        if (strcmp(argv[i], "-n") == 0)
+        {
+            proc = atoi(argv[++i]);
+        }
+        else if (strcmp(argv[i], "-s") == 0)
+        {
+            simul = atoi(argv[++i]);
+        }
+        else if (strcmp(argv[i], "-t") == 0)
+        {
+            iter = argv[++i];
+        }
     }
 
-    int status = -1;
-    waitpid(childPid, &status, WEXITED);
+    // Loop to check for how many children have run.
+    while (completedChilds < proc)
+    {
+        //printf("running childs: %d", runningChilds);
+        // Loop to Check for how many children are currently running.
+        while (runningChilds < simul && (completedChilds + runningChilds) < proc)
+        {
+            pid_t childPid = fork();
+            if (childPid == 0)
+            {
+                char *args[] = {"./user", iter, NULL};
+                execvp(args[0], args);
+            }
+            else
+            {
+                runningChilds++;
+            }
+        }
+    }
+    // Waiting for children to finish
+    pid_t finishedPid = wait(&status);
+    if (finishedPid > 0)
+    {
+        completedChilds++;
+        runningChilds--;
+    }
 
     return EXIT_SUCCESS;
 }
